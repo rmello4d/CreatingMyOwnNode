55476b43109398cb5162c77e34cb5a414b5b9dbc
Ricardo <ricardo.mello@4d.com>
2013-12-04 19:28:14 +0100

    adding class headers and license

--- designer.js
+++ designer.js
@@ -1,9 +1,8 @@
-(function() {
-    var CreatingMyOwnNode = Widget.CreatingMyOwnNode.inherit(WAF.require('waf-behavior/studio'));
+(function(CreatingMyOwnNode) {
 
 	CreatingMyOwnNode.setWidth('180');
 	CreatingMyOwnNode.setHeight('80');
 
-})();
+});
 
 // For more information, refer to http://doc.wakanda.org/Wakanda0.DevBranch/help/Title/en/page3870.html
